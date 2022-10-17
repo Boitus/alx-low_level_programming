80cc2934e0a0b093232bae819e35dceb16419d63
Boitus <bseetsa@gmail.com>
2022-10-17 16:22:13 -0700

    8 first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	for (k = 'a'; k <= 'f'; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
