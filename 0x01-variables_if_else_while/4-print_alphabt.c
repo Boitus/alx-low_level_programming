913dfc9e5e2727f60ffc20c1419bb3524309e8b7
Boitus <bseetsa@gmail.com>
2022-10-17 15:47:30 -0700

    4 first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 'a' ; l <= 'z'; l++)
+	{
+		if (l == 'q' && l == 'e')
+		{
+			continue;
+		}
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
