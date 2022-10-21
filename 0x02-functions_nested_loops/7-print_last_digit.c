199b2566c5aee34c38b73f78640017dde96f8a61
Boitus <bseetsa@gmail.com>
2022-10-21 12:24:00 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Print_last_digit of a number.
+ * @n: input number as an interger.
+ * Return: last_digit.
+ */
+int print_last_digit(int n)
+{
+	int l, p;
+
+	l = n % 10;
+	if (1 < 0)
+		l = -l;
+	p = '0' + l;
+	_putchar(p);
+	return (l);
+}
