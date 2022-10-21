b6cfbfe3fe0bfb4bd65bda7398d4a264d7919238
Boitus <bseetsa@gmail.com>
2022-10-21 02:18:14 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		retur(1);
+		return(1);
 	}
 	else if (n < 0)
 	{
@@ -22,6 +22,6 @@ int print_sign(int n)
 	{
 		_putchar(48);
 		return (0);
-	{
-	_putchar('\n')
+	}
+	_putchar('\n');
 }
