1362858e0a5ae4589428a7eaaf30d43e2c816046
Boitus <bseetsa@gmail.com>
2022-10-26 14:13:55 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * no_more - prints numbers between 0 to 4
+ * 10 times.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, ch;
+
+	for (i = 0; ch < 10; ch++)
+	{
+		 for (ch = 0; ch < 15; ch++)
+		 {
+			 if (ch >= 10)
+				 _putchar(( / 10 + 48);
+			_putchar((ch % 10 + 48);
+                  }
+		  _putchar('\n');
+	}
+}
