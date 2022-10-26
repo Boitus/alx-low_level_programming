e53a3a4b445997317a39225121f31dba1b5a85eb
Boitus <bseetsa@gmail.com>
2022-10-25 22:00:31 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,15 +11,15 @@ void puts_half(char *str)
 
 	while (count >= 0)
 	{
-		if (str[count] == '\')
+		if (str[count] == '\0')
 			break;
 		count++;
 	}
 	if (count % 2 == 1)
 		i = count / 2;
 	else
-		i = (count - 1) /2
+		i = (count - 1) / 2;
 	for (i++; i < count; i++)
-		putchar(str[i]
-	putchar('\n')
+		putchar(str[i]);
+	putchar('\n');
 }
