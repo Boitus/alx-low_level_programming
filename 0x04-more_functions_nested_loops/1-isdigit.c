2c4dd7b55b00960b860e6283eb40a5e7589f8650
Boitus <bseetsa@gmail.com>
2022-10-26 12:03:38 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two itegers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
