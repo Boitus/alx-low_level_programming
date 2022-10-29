c1d0bf871cd37ee93ff435f35a3969cb85c0dc40
Boitus <bseetsa@gmail.com>
2022-10-28 17:25:56 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (count2 < n)
 	{
-		*(dest + count) = *(src + count 2);
+		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
 			break;
 	count++;
