b319d10acc416338f1fea351578dd94d6131e362
Boitus <bseetsa@gmail.com>
2022-10-28 17:07:41 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,15 @@
  */
 char *_strcat(char *dest, char src)
 {
-	int count = 0, count2 = 0;
+	int count = 0;
+	int count2 = 0;
 
 	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
 
-	while (count2 > n)
+	while (count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
 		if (*(src + count2) == '\0')
