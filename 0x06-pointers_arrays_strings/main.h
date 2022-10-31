f49f44fd205323576ee25c28a212cc40d67b9865
Boitus <bseetsa@gmail.com>
2022-10-31 12:13:29 -0700

    damn

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,5 @@ int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
+void print_number(int n);
 #endif
